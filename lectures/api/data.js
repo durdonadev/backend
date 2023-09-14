8b9448f77f4d52a0501b6e77432e324607ca466b
Durdona Ibragimova <durdona.dev@gmail.com>
2023-09-14 00:47:17 -0400

    Create Lecture API

---
+++ lectures/api/data.js
@@ -0,0 +1,9 @@
+const todos = {
+    1: { id: 1, task: "Some text", status: "Todo" }, // "Todo", "InProgress", "Done"
+    2: { id: 2, task: "Some text", status: "Todo" }, // "Todo", "InProgress", "Done"
+    3: { id: 3, task: "Some text", status: "Todo" } // "Todo", "InProgress", "Done"
+};
+
+module.exports = {
+    todos
+};
