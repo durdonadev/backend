94ee530aa7729ed0e782fb50dd4d27900804bca7
Durdona Ibragimova <durdona.dev@gmail.com>
2023-09-15 23:50:57 -0400

    Finish Assignment-4

---
+++ assignments/assignment-4/jira-api-2/data.js
@@ -0,0 +1,38 @@
+import { v4 as uuid } from "uuid";
+
+export const stories = {
+    // "uuid-story-1": {
+    //     id: "uuid-story-1",
+    //     name: "Story 1",
+    //     description: "Description for Story 1",
+    //     status: "Todo", // "Todo", "InProgress", "InQA", "Done"
+    //     tasks: {
+    //         "uuid-task-1": {
+    //             id: "uuid-task-1",
+    //             name: "Sub-task 1",
+    //             description: "Description for Sub-task 1",
+    //             status: "Todo" // "Todo", "InProgress", "Done"
+    //         },
+    //         "uuid-task-2": {
+    //             id: "uuid-task-2",
+    //             name: "Sub-task 2",
+    //             description: "Description for Sub-task 2",
+    //             status: "InProgress" // "Todo", "InProgress", "Done"
+    //         }
+    //     }
+    // },
+    // "uuid-story-2": {
+    //     id: "uuid-story-2",
+    //     name: "Story 2",
+    //     description: "Description for Story 2",
+    //     status: "InProgress", // "Todo", "InProgress", "InQA", "Done"
+    //     tasks: {
+    //         "uuid-task-3": {
+    //             id: "uuid-task-3",
+    //             name: "Sub-task 3",
+    //             description: "Description for Sub-task 3",
+    //             status: "Done" // "Todo", "InProgress", "Done"
+    //         }
+    //     }
+    // }
+};
