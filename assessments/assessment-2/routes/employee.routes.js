f384c69eea51fce16aa32d77993fb6ffd5b76215
Durdona Ibragimova <durdona.dev@gmail.com>
2023-09-20 23:44:47 -0400

    Add utils and const to Assessment 2

--- assessments/assessment-2/routes/employee.routes.js
+++ assessments/assessment-2/routes/employee.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { employeeController } from "../controllers/employee.controller.js";
-import { ValidationMiddleware } from "../middlewares/validation.middleware.js";
+import { validationMiddleware } from "../middlewares/validation.middleware.js";
 
 const employeeRouter = Router();
 
@@ -13,18 +13,18 @@ const employeeRouter = Router();
 employeeRouter.get("/", employeeController.getAllEmployees);
 employeeRouter.get(
     "/:employeeId",
-    ValidationMiddleware.validateEmployeeIds,
+    validationMiddleware.validateEmployeeIds,
     employeeController.getEmployeeById
 );
 employeeRouter.post("/", employeeController.createEmployee);
 employeeRouter.put(
     "/:employeeId",
-    ValidationMiddleware.validateEmployeeIds,
+    validationMiddleware.validateEmployeeIds,
     employeeController.updateEmployee
 );
 employeeRouter.delete(
     "/:employeeId",
-    ValidationMiddleware.validateEmployeeIds,
+    validationMiddleware.validateEmployeeIds,
     employeeController.deleteEmployee
 );
 
