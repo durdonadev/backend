97bf903d4f6a606bcc7920880133064d96d182fb
Durdona Ibragimova <durdona.dev@gmail.com>
2023-09-24 02:33:19 -0400

    Fix directory typo

--- lectures/book-app-api-express-service-middleware/const/allowedFields.js
+++
@@ -1,2 +0,0 @@
-export const BOOK_FIELDS = ["author", "genre", "name"];
-export const REVIEW_FIELDS = ["reviewer", "text", "rating"];
--- lectures/book-app-api-express-service-middleware/utils/sanitizeObj.js
+++
@@ -1,8 +0,0 @@
-export const sanitizeObj = (allowedField, obj) => {
-    for (const field in obj) {
-        if (allowedField.includes(field)) {
-            sanitizedData[field] = obj[field];
-        }
-    }
-    return sanitizedData;
-};
